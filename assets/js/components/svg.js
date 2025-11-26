e3d6e52f45f8955d090e06a9d57db44e6a1d930a
Lucas Petit <lucas.noirotorion@gmail.com>
2025-11-26 15:59:54 +0100

    Ajout de la page de projets academiques

---
+++ assets/js/components/svg.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const arrowForwardIconSVG = `<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+                                    <path d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
+                                 </svg>`;
+    
+    const arrowForwardIconAll = document.querySelectorAll('.arrowForwardIcon');
+
+    arrowForwardIconAll.forEach(span => {
+        span.innerHTML = arrowForwardIconSVG;
+    });
+});
