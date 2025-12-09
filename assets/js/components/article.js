8cedfbaaf6c509db28697a2a08d60a2924fc3809
Lucas Petit <lucas.noirotorion@gmail.com>
2025-12-09 17:36:13 +0100

    encore des chapit'res

--- assets/js/components/article.js
+++ assets/js/components/article.js
@@ -1,7 +1,7 @@
 // faire en sorte d'avoir un truc qui ajoute tous seul les span label partout
 
 document.addEventListener("DOMContentLoaded", () => {
-    const hoho = document.querySelectorAll('.example, .counterexample, .conjecture, .definition, .remark, .theorem, .proposition, .lemma, .problem, .notation');
+    const hoho = document.querySelectorAll('.example, .counterexample, .proof, .conjecture, .definition, .remark, .theorem, .proposition, .lemma, .problem, .notation');
     hoho.forEach( example => {
         example.insertAdjacentHTML("afterbegin", `
             <span class="label"></span>
